e71fa5e0514a01774e1b40946bcac5bb9bde0c94
quinnc <wankwan@163.com>
2019-04-24 23:05:45 +0800

    initial project

---
+++ MainForm.Designer.cs
@@ -0,0 +1,172 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: quinnc
+ * Date: 2019/4/23
+ * Time: 21:18
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace read
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.NotifyIcon notifyIcon1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.button1 = new System.Windows.Forms.Button();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.button2 = new System.Windows.Forms.Button();
+			this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.button3 = new System.Windows.Forms.Button();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(559, 12);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "打开文件";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// timer1
+			// 
+			this.timer1.Tick += new System.EventHandler(this.Timer1Tick);
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			this.openFileDialog1.Filter = "txt files(*.txt)|*.txt|All files(*.*)|*.*";
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(64, 23);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "文件路径";
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(82, 9);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(471, 23);
+			this.label2.TabIndex = 2;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(559, 59);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 3;
+			this.button2.Text = "开始任务";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// notifyIcon1
+			// 
+			this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+			this.notifyIcon1.Text = "notifyIcon1";
+			this.notifyIcon1.Visible = true;
+			this.notifyIcon1.Click += new System.EventHandler(this.NotifyIcon1DoubleClick);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(128, 45);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(160, 21);
+			this.textBox1.TabIndex = 4;
+			this.textBox1.Text = "宽带连接";
+			// 
+			// label3
+			// 
+			this.label3.Location = new System.Drawing.Point(13, 45);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(100, 23);
+			this.label3.TabIndex = 5;
+			this.label3.Text = "拨号名称";
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(559, 95);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 23);
+			this.button3.TabIndex = 7;
+			this.button3.Text = "停止任务";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.Button3Click);
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.Location = new System.Drawing.Point(13, 130);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.Size = new System.Drawing.Size(602, 119);
+			this.richTextBox1.TabIndex = 8;
+			this.richTextBox1.Text = "";
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(674, 261);
+			this.Controls.Add(this.richTextBox1);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button1);
+			this.Name = "MainForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "一个小时执行一次";
+			this.SizeChanged += new System.EventHandler(this.MainFormSizeChanged);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ MainForm.cs
@@ -0,0 +1,144 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: quinnc
+ * Date: 2019/4/23
+ * Time: 21:18
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+using System.Text;
+using System.Diagnostics;
+
+namespace read
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			this.timer1.Interval = 1000;
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		private List<String[]> accountList = new List<string[]>();
+		
+		private int len = 0;
+		
+		private int count = 0;
+		
+		private Boolean first = true;
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+			DialogResult result = this.openFileDialog1.ShowDialog();
+			if(result == DialogResult.OK){
+				this.label2.Text = this.openFileDialog1.FileName;
+			}
+		}
+		
+		void Timer1Tick(object sender, EventArgs e)
+		{
+			
+			if(accountList != null && len > 0){
+				if(count == len){
+					count = 0;
+				}
+				String[] myacct = accountList[count];
+				count++;
+				String name = this.textBox1.Text;
+				String command = "rasdial "+name+" "+myacct[0]+" "+ myacct[1];
+//				String command = "echo "+myacct[0]+" "+ myacct[1];
+				this.richTextBox1.AppendText(DateTime.Now.ToString()+ "   ");
+				this.richTextBox1.AppendText(command);
+				this.richTextBox1.AppendText("\n");
+				String aa = RunCmd(command);
+				this.richTextBox1.AppendText("连接结果：" + aa);
+			}
+			if(first){
+				first = false;
+				this.timer1.Interval = 3600000;
+			}
+			
+			
+		}
+		void MainFormSizeChanged(object sender, EventArgs e)
+		{
+			if (this.WindowState == FormWindowState.Minimized)
+			{
+				this.Hide();   //隐藏窗体
+				notifyIcon1.Visible = true; //使托盘图标可见
+			}
+		}
+		void NotifyIcon1DoubleClick(object sender, EventArgs e)
+		{
+			this.Visible = true;
+			this.WindowState = FormWindowState.Normal;
+			
+		}
+		void Button2Click(object sender, EventArgs e)
+		{
+			String files = this.label2.Text;
+			using(FileStream fin = new FileStream(files, FileMode.Open)){
+				using(StreamReader reader = new StreamReader(fin, Encoding.UTF8)){
+					while(!reader.EndOfStream){
+						String[] content = reader.ReadLine().Split(new char[1]{','});
+						accountList.Add(content);
+					}
+					len = accountList.Count;
+				}
+			}
+			
+			this.timer1.Start();
+			richTextBox1.AppendText("任务启动。。。。。。。\n");
+		}
+		
+		private string RunCmd(string command)
+		{
+			//实例一个Process类，启动一个独立进程
+			Process p = new Process();
+			//Process类有一个StartInfo属性
+			//设定程序名
+			p.StartInfo.FileName = "cmd.exe";
+			//设定程式执行参数
+			p.StartInfo.Arguments = "/c " + command;
+			//关闭Shell的使用
+			p.StartInfo.UseShellExecute = false;
+			//重定向标准输入
+			p.StartInfo.RedirectStandardInput = true;
+			p.StartInfo.RedirectStandardOutput = true;
+			//重定向错误输出
+			p.StartInfo.RedirectStandardError = true;
+			//设置不显示窗口
+			p.StartInfo.CreateNoWindow = true;
+			//启动
+			p.Start();
+			//也可以用这种方式输入要执行的命令
+			//不过要记得加上Exit要不然下一行程式执行的时候会当机
+			//p.StandardInput.WriteLine(command);
+			//p.StandardInput.WriteLine("exit");
+			//从输出流取得命令执行结果
+			return p.StandardOutput.ReadToEnd();
+		}
+		void Button3Click(object sender, EventArgs e)
+		{
+			this.timer1.Stop();
+			this.first = false;
+			this.timer1.Interval = 100;
+			this.accountList.Clear();
+		}
+	}
+}
